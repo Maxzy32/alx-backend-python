767233ac803bfd96f9c4b03074e0b5db45259340
CaptaiRich <ahiakpormaxwell32@gmail.com>
2025-07-28 08:22:45 +0000

    Commit changes

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -2,11 +2,12 @@ import django_filters
 from .models import Message
 from django_filters import rest_framework as filters
 
+
 class MessageFilter(filters.FilterSet):
-    sent_after = filters.DateTimeFilter(field_name="sent_at", lookup_expr='gte')
-    sent_before = filters.DateTimeFilter(field_name="sent_at", lookup_expr='lte')
-    sender_email = filters.CharFilter(field_name="sender__email", lookup_expr='iexact')
+    sent_after = filters.DateTimeFilter(field_name='sent_at', lookup_expr='gte')
+    sent_before = filters.DateTimeFilter(field_name='sent_at', lookup_expr='lte')
+    sender = filters.UUIDFilter(field_name='sender__user_id')
 
     class Meta:
         model = Message
-        fields = ['sent_after', 'sent_before', 'sender_email']
+        fields = ['sender', 'sent_after', 'sent_before']
