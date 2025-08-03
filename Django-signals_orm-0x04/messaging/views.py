aec7c213817ea5ee25c294c20a029a37414c05e7
CaptaiRich <ahiakpormaxwell32@gmail.com>
2025-08-03 04:47:34 +0000

    c

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -4,6 +4,14 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
+from messaging_app.chats.serializers import MessageSerializer 
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .models import Message
+from messaging_app.chats.serializers import MessageSerializer
+
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Message
 
@@ -21,3 +29,30 @@ def get_conversation_queryset():
         .prefetch_related('replies')
         .all()
     )
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Message.objects.select_related('sender', 'receiver', 'parent_message') \
+                              .prefetch_related('replies') \
+                              .filter(receiver=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+def get_threaded_replies(request, message_id):
+    """Returns a message and all its replies recursively"""
+    message = get_object_or_404(Message, id=message_id)
+
+    def fetch_replies(msg):
+        replies = []
+        for r in msg.replies.all():
+            replies.append(r)
+            replies += fetch_replies(r)
+        return replies
+
+    replies = fetch_replies(message)
+    serialized = MessageSerializer([message] + replies, many=True)
+    return Response(serialized.data)
